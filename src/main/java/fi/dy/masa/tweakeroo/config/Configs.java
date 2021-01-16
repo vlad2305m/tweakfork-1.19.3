4582fe9ea32f6ecac9b8624ce5d829ae19d146d3
Andrews54757 <andrews54757@gmail.com>
2021-01-16 15:08:38 -0500

    Add sidescoreboard disable

--- src/main/java/fi/dy/masa/tweakeroo/config/Configs.java
+++ src/main/java/fi/dy/masa/tweakeroo/config/Configs.java
@@ -252,6 +252,7 @@ public class Configs implements IConfigHandler
         public static final ConfigBooleanHotkeyed       DISABLE_PORTAL_GUI_CLOSING      = new ConfigBooleanHotkeyed("disablePortalGuiClosing",              false, "", "If enabled, then you can still open GUIs while in a Nether Portal");
         public static final ConfigBooleanHotkeyed       DISABLE_RAIN_EFFECTS            = new ConfigBooleanHotkeyed("disableRainEffects",                   false, "", "Disables rain rendering and sounds");
         public static final ConfigBooleanHotkeyed       DISABLE_RENDER_DISTANCE_FOG     = new ConfigBooleanHotkeyed("disableRenderDistanceFog",             false, "", "Disables the fog that increases around the render distance");
+        public static final ConfigBooleanHotkeyed       DISABLE_SIDESCOREBOARD          = new ConfigBooleanHotkeyed("disableSideScoreboard",                false, "", "Disables sidebar scoreboard rendering");
         public static final ConfigBooleanHotkeyed       DISABLE_SIGN_GUI                = new ConfigBooleanHotkeyed("disableSignGui",                       false, "", "Prevent the Sign edit GUI from opening");
         public static final ConfigBooleanHotkeyed       DISABLE_SLIME_BLOCK_SLOWDOWN    = new ConfigBooleanHotkeyed("disableSlimeBlockSlowdown",            false, "", "Removes the slowdown from walking on Slime Blocks.\n(This is originally from usefulmod by nessie.)");
         public static final ConfigBooleanHotkeyed       DISABLE_TILE_ENTITY_RENDERING   = new ConfigBooleanHotkeyed("disableTileEntityRendering",           false, "", "Prevents all TileEntity renderers from rendering");
@@ -280,6 +281,7 @@ public class Configs implements IConfigHandler
                 DISABLE_PORTAL_GUI_CLOSING,
                 DISABLE_RAIN_EFFECTS,
                 DISABLE_RENDER_DISTANCE_FOG,
+                DISABLE_SIDESCOREBOARD,
                 DISABLE_SIGN_GUI,
                 DISABLE_SLIME_BLOCK_SLOWDOWN,
                 DISABLE_TILE_ENTITY_RENDERING,

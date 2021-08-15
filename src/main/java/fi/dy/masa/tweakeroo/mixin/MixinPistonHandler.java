a218f060b1fa3a140934602ff207e35e612d5cb9
Andrew S <Andrews54757@gmail.com>
2021-08-15 12:27:02 -0400

    Optional mixin

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinPistonHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinPistonHandler.java
@@ -9,9 +9,10 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(PistonHandler.class)
 public abstract class MixinPistonHandler
 {
-	@ModifyConstant(method = "tryMove", constant = @Constant(intValue = 12), require = 3, allow = 3)
+	@ModifyConstant(method = "tryMove", constant = @Constant(intValue = 12), require = 0, allow = 3)
 	private int modifyPushLimitPistorderMod(int value)
 	{
 		return PistonUtils.getPushLimit();
 	}
 }
+
